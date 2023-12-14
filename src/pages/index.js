95776d868e7ac17fab5c980215ab818ae9d4dd0e
Scott Gruber <scott.gruber@rccsinc.local>
2023-12-13 20:10:41 -0500

    Finished chapter 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
     <main>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage src="../images/mets.png" alt="Mets"/>
     </main>
     </Layout>
   )
