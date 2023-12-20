eaaf1b0a300b08dd8dbf0ae419889ce1f9177c36
Scott Gruber <scott.gruber@rccsinc.local>
2023-12-20 14:03:55 -0500

    MDX Tutorial

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,10 +8,12 @@ const BlogPage = ({data}) => {
     <Layout pageTitle="My Blog Posts">
       <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
+          data.allMdx.nodes.map(node => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
           ))
         }
       </ul>
@@ -22,9 +24,15 @@ const BlogPage = ({data}) => {
 
 export const query = graphql`
 query{
-  allFile{
-    nodes{
-      name
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      id
+      excerpt
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
     }
   }
 }
